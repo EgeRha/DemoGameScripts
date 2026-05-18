0b63addaa8594cb93a293428f357e979b7507433
EgeRha <rhaegarrh99r@gmail.com>
2026-05-18 07:41:14 +0300

    Create DoorTransition.cs

---
+++ DoorTransition.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTransition : MonoBehaviour
+{
+    public string nextSceneName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
+
